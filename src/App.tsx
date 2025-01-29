e98067f3077860c0e199ba8dcef4965153289035
ChristAI <158454707+ZetaPepe@users.noreply.github.com>
2025-01-30 03:16:02 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App(): JSX.Element {
   const [lerpFactor, setLerpFactor] = useState<number>(0.5)
   const [openDrawer, setOpenDrawer] = useState<boolean>(false)
   const [activeTab, setActiveTab] = useState<string>("")
-  const [selectedModel, setSelectedModel] = useState<string>("深空之眼-托特")
+  const [selectedModel, setSelectedModel] = useState<string>("Eye of Deep Space - Brahma 2")
   const [selectedBackground, setSelectedBackground] = useState<string>("Static")
   const [isPlaying, setIsPlaying] = useState<boolean>(false)
   const [selectedAnimation, setSelectedAnimation] = useState<string>("")
