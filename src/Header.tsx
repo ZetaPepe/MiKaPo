17f1260473cce017c1a00f7f3fae1d83a128b275
ChristAI <158454707+ZetaPepe@users.noreply.github.com>
2025-01-31 14:58:04 +0800

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,5 @@
 import { Avatar, IconButton } from "@mui/material"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// 导入新的 Telegram 和 Twitter 图标
 import { faGithub, faTelegram, faTwitter } from "@fortawesome/free-brands-svg-icons"
 
 function Header(): JSX.Element {
@@ -20,10 +19,20 @@ function Header(): JSX.Element {
             },
           }}
         />
-        <h2>Mikiu</h2>
+        {/* 修改字体样式为艺术体，并设置为白色 */}
+        <h2
+          style={{
+            fontFamily: "'Brush Script MT', cursive", // 使用艺术字体
+            fontSize: "2.5rem", // 字体大小
+            color: "white", // 字体颜色设置为白色
+            fontWeight: "normal", // 字体重量为正常
+            letterSpacing: "2px", // 字母间距
+          }}
+        >
+          Mikiu
+        </h2>
       </div>
 
-      {/* 添加自定义 margin-top 来调整 CA 文本与顶部的距离 */}
       <div className="header-item" style={{ marginTop: "50px" }}>
         <p> </p>
       </div>
@@ -34,13 +43,11 @@ function Header(): JSX.Element {
             <FontAwesomeIcon icon={faGithub} color="white" size="sm" />
           </IconButton>
         </a>
-        {/* 将 Github 图标替换为 Telegram 图标 */}
         <a href="https://t.me/your-telegram-link" target="_blank">
           <IconButton>
             <FontAwesomeIcon icon={faTelegram} color="white" size="sm" />
           </IconButton>
         </a>
-        {/* 将 Download 图标替换为 Twitter 图标 */}
         <a href="https://x.com/mikiuai" target="_blank">
           <IconButton size="small" color="inherit">
             <FontAwesomeIcon icon={faTwitter} color="white" size="sm" />
