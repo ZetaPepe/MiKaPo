cbcebe2089ac40e570fc007e52447a4c1c8711b6
ChristAI <158454707+ZetaPepe@users.noreply.github.com>
2025-01-31 15:20:46 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -101,8 +101,8 @@ function App(): JSX.Element {
             }}
             style={{
               marginTop: "15px",
-              padding: "10px 35px",
-              fontSize: "30px",
+              padding: "5px 17px",
+              fontSize: "15px",
               fontWeight: "bold",
               color: "white",
               backgroundColor: "#007bff",
