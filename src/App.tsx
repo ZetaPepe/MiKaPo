3f7cacecbe94a9736e85e319cad21970ba04807d
ChristAI <158454707+ZetaPepe@users.noreply.github.com>
2025-02-05 10:48:33 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -89,8 +89,10 @@ function App(): JSX.Element {
 
           <p style={{ margin: "0", fontSize: "14px", lineHeight: "1.5", maxWidth: "350px" }}>
             First 3D AI Agent Platform
-          </p>
-
+          <p style={{ margin: "10px 0 0", fontSize: "14px", lineHeight: "1.5", maxWidth: "350px" }}>
+            CA: DbsPqgSoPjF8VuDpKiekQdntbNUM7K4Fo3VrvjFPpump
+          </p >
+            
           <button
             onClick={() => {
               setOpacity(0);
